9dc72a36d4d3b4179068d07992cf518a620eda2f
YangXvJie <2181264773@qq.com>
2026-01-07 20:47:08 +0800

    1

---
+++ HotFix/UI/RentWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+
+namespace HotFix
+{
+    class RentWindow:Window
+    {
+        Button closeBtn;
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null)
+        {
+            base.Awake(param1, param2, param3);
+            closeBtn = m_Transform.Find("BackImg/CloseBtn").GetComponent<Button>();
+            AddButtonClickListener(closeBtn, () =>
+            {
+                UIManager.instance.CloseWnd(this);
+            });
+        }
+    }
+}
---
+++ HotFix/UI/ShareWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HotFix
+{
+    public class ShareWindow : Window
+    {
+        private Button ReturnBtn;
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null)
+        {
+            GatAllComponent();
+            AddAllBtnListener();
+        }
+
+        private void AddAllBtnListener()
+        {
+            AddButtonClickListener(ReturnBtn, SHAREPANEL);
+        }
+
+        private void SHAREPANEL()
+        {
+            UIManager.instance.CloseWnd(FilesName.SHAREPANEL);
+        }
+
+        private void GatAllComponent()
+        {
+            ReturnBtn= m_Transform.Find("Back/ReturnBtn").GetComponent<Button>();
+        }
+    }
+}
