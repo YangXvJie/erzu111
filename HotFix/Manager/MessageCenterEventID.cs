954f02c544cc0a86b727cabddead7fb191a1db74
AIQIYA\aiqiy <aiqiya.qixing@qq.com>
2026-01-07 20:48:15 +0800

    新

---
+++ HotFix/Manager/MessageCenterEventID.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotFix
+{
+    /// <summary>
+    /// 消息中心
+    /// 消息中心
+    /// </summary>
+   public class MessageCenterEventID
+    {
+       
+        public const int PlayerChangePosition = 1001;
+    }
+}
+
+
---
+++ HotFix/UI/HorseFeedWindow.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using UnityEngine.UI;
+
+namespace HotFix
+{
+    internal class HorseFeedWindow : Window
+    {
+        private Button closeBtn;
+
+
+
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null)
+        {
+            GatAllComponent();
+            AddAllBtnListener();
+        }
+        void AddAllBtnListener()
+        {
+
+            AddButtonClickListener(closeBtn, ClosePanel);
+
+        }
+        private void ClosePanel()
+        {
+            // 确保正确关闭当前窗口
+            UIManager.instance.CloseWnd(FilesName.HORSEFEEDPANEL);
+        }
+        private void GatAllComponent()
+        {
+            closeBtn = m_Transform.Find("CloseBtn").GetComponent<Button>();
+        }
+    }
+}
