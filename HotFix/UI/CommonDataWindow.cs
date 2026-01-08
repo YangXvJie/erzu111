b109953a9602699ed14d080122662d88b9ce68c9
AIQIYA\aiqiy <aiqiya.qixing@qq.com>
2026-01-08 18:50:37 +0800

    打开

---
+++ HotFix/UI/CommonDataWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotFix
+{
+    class CommonDataWindow:Window
+    {
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null)
+        {
+           
+        }
+        public override void OnShow(object param1 = null, object param2 = null, object param3 = null)
+        {
+            
+        }
+    }
+}
